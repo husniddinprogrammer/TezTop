3ef9ace54b16fb398800f728f5d82786b8ec68f5
husniddinprogrammer <husniddinprogrammer@gmail.com>
2021-10-06 21:30:41 +0500

    Tez-Top

---
+++ src/main/java/Husniddin/TezTopApplication.java
@@ -0,0 +1,13 @@
+package Husniddin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TezTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TezTopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Husniddin/TezTopApplicationTests.java
@@ -0,0 +1,13 @@
+package Husniddin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TezTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
